a0816f89ce27e7d521f2dfc94d6c7ddaf7b0244e
Blankitty <kjs74962880@gmail.com>
2025-07-02 15:58:26 +0900

    Update 9_3확인문제.java

--- java/연습문제/9_3확인문제.java
+++ java/연습문제/9_3확인문제.java
@@ -1,1 +1,40 @@
+package ch07.sec03;
 
+public class CheckBox {
+    OnSelectListener listener;
+
+    void setOnSelectListener(OnSelectListener listener) {
+        this.listener = listener;
+    }
+
+    void select() {
+        listener.onSelect();
+    }
+
+    static interface OnSelectListener {
+        void onSelect();
+        
+    }
+
+}
+```
+package ch07.sec03;
+
+public class CheckBoxExample {
+    public static void main(String[] args) {
+        CheckBox checkBox = new CheckBox();
+        checkBox.setOnSelectListener(
+            new CheckBox.OnSelectListener() {
+                @Override
+                public void onSelect() {
+                    System.out.println("배경을 변경합니다.");
+                }
+            }
+
+        );
+    
+        checkBox.select();
+    }
+}
+
+            
