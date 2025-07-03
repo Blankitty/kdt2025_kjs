d6e1429bb51b3d6e67bdcee362067e6bffffd18d
Blankitty <kjs74962880@gmail.com>
2025-07-03 18:01:24 +0900

    Create 11_3확인문제.java

---
+++ java/연습문제/11_3확인문제.java
@@ -0,0 +1,44 @@
+package ch11.exam03;
+
+public class Student {
+    private String studentNum;
+
+    public Student(String studentNum) {
+        this.studentNum = studentNum;
+    }
+
+    public String getStudentNum() {
+        return studentNum;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if( obj instanceof Student) {
+            Student compareStudent = (Student) obj;
+            if(this.studentNum == compareStudent.studentNum) {
+                return true;
+            }
+        }
+        return false; 
+    }
+    @Override
+    public int hashCode() {
+        return studentNum.hashCode();
+    }
+    
+}
+```
+package ch11.exam03;
+
+import java.util.HashMap;
+
+public class StudentExample {
+    public static void main(String[] args) {
+        HashMap<Student, String> hashMap = new HashMap<Student, String>();
+
+        hashMap.put(new Student("1"),"95");
+
+        String score = hashMap.get(new Student("1"));
+        System.out.println("1번 학생의 총점 : " + score);
+    }
+
+}
