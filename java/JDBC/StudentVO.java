8476521a26368425360738549fcef88a2b2fa96f
Blankitty <kjs74962880@gmail.com>
2025-07-10 20:07:15 +0900

    Create StudentVO.java

---
+++ java/JDBC/StudentVO.java
@@ -0,0 +1,54 @@
+package exam.vo;
+
+public class StudentVO {
+    private int stdno;
+    private String stdname;
+    private String phone;
+    private String email;
+
+    public StudentVO() {}
+
+    public StudentVO(int stdno, String stdname, String phone, String email) {
+        this.stdno = stdno;
+        this.stdname = stdname;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public int getStdno() {
+        return stdno;
+    }
+
+    public void setStdno(int stdno) {
+        this.stdno = stdno;
+    }
+
+    public String getStdname() {
+        return stdname;
+    }
+
+    public void setStdname(String stdname) {
+        this.stdname = stdname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return stdno + " | " + stdname + " | " + phone + " | " + email;
+    }
+}
